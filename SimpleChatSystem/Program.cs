15745dfa603ee034b69b51eab5ae6c43ffa64aef
Max Akbar <maximum2000@hotmail.com>
2025-01-14 18:07:02 -0800

    simple updates

--- SimpleChatSystem/Program.cs
+++ SimpleChatSystem/Program.cs
@@ -10,54 +10,39 @@ var ollamaClient = new HttpClient
 };
 var modeName = await SelectOllamaModel(ollamaClient);
 
+await StartChat(ollamaClient, modeName);
+Console.WriteLine("Exiting the application.");
 
-if (modeName != string.Empty)
-{
-    // chat with the model
-    Console.WriteLine("Chatting with the model...");
-    Console.WriteLine();
-
-    var chatRequest = new ChatRequest
-    {
-        Model = modeName,
-        Messages = [],
-        Stream = false
-    };
-    var userMessage = new Message { Role = "system", Content = "You are a helpfull assistant."};
-
-    chatRequest.Messages.Add(userMessage);
-
-    while (true)
-    {
-        await ChatWithModel(ollamaClient, chatRequest);
-    }
-}
-
-static async Task ChatWithModel(HttpClient ollamaClient, ChatRequest chatRequest)
+static async Task<bool> ChatWithModel(HttpClient ollamaClient, ChatRequest chatRequest)
 {
     Console.Write("User > ");
 
+    var endOfConversation = false;
     var userInput = Console.ReadLine();
-    var userMessage = new Message { Role = "user", Content = userInput };
 
-    chatRequest.Messages.Add(userMessage);
+    if (userInput == "/bye" || string.IsNullOrWhiteSpace(userInput))
+    {
+        return endOfConversation = false;
+    }
 
-    var chatRequestJson = JsonSerializer.Serialize(chatRequest);
-    var content = new StringContent(chatRequestJson, Encoding.UTF8, "application/json");
-    var responseMessage = await ollamaClient.PostAsync("/api/chat", content);
-    var llmResponse = await responseMessage.Content.ReadAsStringAsync();
-    var chatResponse = JsonSerializer.Deserialize<ChatResponse>(llmResponse);
+    ChatResponse? chatResponse = await ChatCompletion(ollamaClient, chatRequest, userInput);
 
     if (chatResponse != null)
     {
         var assistantMessage = new Message { Role = chatResponse.Message.Role, Content = chatResponse.Message.Content };
         chatRequest.Messages.Add(assistantMessage);
         Console.WriteLine($"{assistantMessage.Role} > {assistantMessage.Content}");
+
+        endOfConversation = true;
     }
     else
     {
         Console.WriteLine("Failed to deserialize the response.");
+
+        endOfConversation = false;
     }
+
+    return endOfConversation;
 }
 
 static async Task<string> SelectOllamaModel(HttpClient ollamaClient)
@@ -65,10 +50,10 @@ static async Task<string> SelectOllamaModel(HttpClient ollamaClient)
     var responseMessage = await ollamaClient.GetAsync("/api/tags");
     var content = await responseMessage.Content.ReadAsStringAsync();
 
-    if (responseMessage.StatusCode == HttpStatusCode.OK && content != null)
+    if (responseMessage != null && responseMessage.StatusCode == HttpStatusCode.OK && content != null)
     {
         // Deserialize the JSON string to the ModelsResponse object
-        ModelsResponse modelsResponse = JsonSerializer.Deserialize<ModelsResponse>(content);
+        ModelsResponse modelsResponse = JsonSerializer.Deserialize<ModelsResponse>(content)!;
 
         if (modelsResponse != null)
         {
@@ -98,3 +83,41 @@ static async Task<string> SelectOllamaModel(HttpClient ollamaClient)
     return string.Empty;
 }
 
+static async Task StartChat(HttpClient ollamaClient, string modeName)
+{
+    if (modeName != string.Empty)
+    {
+        // chat with the model
+        Console.Clear();
+        // prompt the user if they want to chat with the model or a knowledge base
+        Console.WriteLine("Hello I am a friendly AI assistant. How can I help you?");
+        Console.WriteLine("To end the chat type /bye");
+        Console.WriteLine();
+
+        var chatRequest = new ChatRequest
+        {
+            Model = modeName,
+            Messages = [],
+            Stream = false
+        };
+        var userMessage = new Message { Role = "system", Content = "You are a helpfull assistant." };
+
+        chatRequest.Messages.Add(userMessage);
+
+        while (await ChatWithModel(ollamaClient, chatRequest)) ;
+    }
+}
+
+static async Task<ChatResponse?> ChatCompletion(HttpClient ollamaClient, ChatRequest chatRequest, string userInput)
+{
+    var userMessage = new Message { Role = "user", Content = userInput };
+
+    chatRequest.Messages.Add(userMessage);
+
+    var chatRequestJson = JsonSerializer.Serialize(chatRequest);
+    var content = new StringContent(chatRequestJson, Encoding.UTF8, "application/json");
+    var responseMessage = await ollamaClient.PostAsync("/api/chat", content);
+    var llmResponse = await responseMessage.Content.ReadAsStringAsync();
+    var chatResponse = JsonSerializer.Deserialize<ChatResponse>(llmResponse);
+    return chatResponse;
+}
